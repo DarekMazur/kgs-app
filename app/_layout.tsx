b4fe8e65c7f6aeda0a65425537e87123cfb631cf
Darek Mazur <dev@darekmazur.it>
2024-09-25 00:52:17 +0200

    Add code formatters

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,18 @@
-import {SplashScreen, Stack} from "expo-router";
-import {useFonts} from "expo-font";
-import {useEffect} from "react";
+import { SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
 
-export default function RootLayout() {
+const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
-    "Montserrat-Black": require("../assets/fonts/Montserrat-Black.ttf"),
-    "Montserrat-Bold": require("../assets/fonts/Montserrat-Bold.ttf"),
-    "Montserrat-ExtraBold": require("../assets/fonts/Montserrat-ExtraBold.ttf"),
-    "Montserrat-ExtraLight": require("../assets/fonts/Montserrat-ExtraLight.ttf"),
-    "Montserrat-Light": require("../assets/fonts/Montserrat-Light.ttf"),
-    "Montserrat-Medium": require("../assets/fonts/Montserrat-Medium.ttf"),
-    "Montserrat-Regular": require("../assets/fonts/Montserrat-Regular.ttf"),
-    "Montserrat-SemiBold": require("../assets/fonts/Montserrat-SemiBold.ttf"),
-    "Montserrat-Thin": require("../assets/fonts/Montserrat-Thin.ttf"),
+    'Montserrat-Black': require('../assets/fonts/Montserrat-Black.ttf'),
+    'Montserrat-Bold': require('../assets/fonts/Montserrat-Bold.ttf'),
+    'Montserrat-ExtraBold': require('../assets/fonts/Montserrat-ExtraBold.ttf'),
+    'Montserrat-ExtraLight': require('../assets/fonts/Montserrat-ExtraLight.ttf'),
+    'Montserrat-Light': require('../assets/fonts/Montserrat-Light.ttf'),
+    'Montserrat-Medium': require('../assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-Regular': require('../assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat-SemiBold': require('../assets/fonts/Montserrat-SemiBold.ttf'),
+    'Montserrat-Thin': require('../assets/fonts/Montserrat-Thin.ttf'),
   });
 
   useEffect(() => {
@@ -33,7 +33,9 @@ export default function RootLayout() {
 
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name='index' options={{ headerShown: false }} />
     </Stack>
   );
-}
+};
+
+export default RootLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
-import {Text} from "react-native";
-import {StatusBar} from "expo-status-bar";
-import {SafeAreaView} from "react-native-safe-area-context";
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Index() {
+const Index = () => {
   return (
     <SafeAreaView className='bg-primaryBG justify-center items-center h-full p-4 font-mtregular'>
-      <Text className='text-primary text-2xl'>Open /app/index.tsx to start working <Text className='text-secondary font-mtblack'>on your app!</Text></Text>
-      <StatusBar style="auto" />
+      <Text className='text-primary text-2xl'>
+        Open /app/index.tsx to start working{' '}
+        <Text className='text-secondary font-mtblack'>on your app!</Text>
+      </Text>
     </SafeAreaView>
   );
-}
+};
+
+export default Index;
