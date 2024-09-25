1e33c72d6ba009c34be6855465a04049890c7961
Darek Mazur <dev@darekmazur.it>
2024-09-26 01:29:45 +0200

    Prepare home screen (#2)

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const signIn = () => {
+  return (
+    <View>
+      <Text>Lorem Ipsum</Text>
+    </View>
+  );
+};
+
+export default signIn;
---
+++ constants/images.ts
@@ -0,0 +1,6 @@
+import logoB from '../assets/images/logoB.png';
+import logoW from '../assets/images/logoW.png';
+import logoFullW from '../assets/images/logoFullW.png';
+import logoFullB from '../assets/images/logoFullB.png';
+
+export default { logoB, logoW, logoFullW, logoFullB };
