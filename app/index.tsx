4989d8dc23bee09069ef10af37c04ff223fbb049
Darek Mazur <dev@darekmazur.it>
2024-09-26 01:29:09 +0200

    Prepare home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,44 @@
-import { Text } from 'react-native';
+import { Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { router } from 'expo-router';
+import { images } from '../constants';
 
 const Index = () => {
   return (
-    <SafeAreaView className='bg-primaryBG justify-center items-center h-full p-4 font-mtregular'>
-      <Text className='text-primary text-2xl'>
-        Open /app/index.tsx to start working{' '}
-        <Text className='text-secondary font-mtblack'>on your app!</Text>
-      </Text>
+    <SafeAreaView className='bg-primaryBG h-full'>
+      <ScrollView
+        contentContainerStyle={{
+          height: '100%',
+        }}
+      >
+        <View className='w-full flex justify-center items-center h-full px-4'>
+          <Image
+            source={images.logoFullW}
+            className='w-full h-[84px] m-6'
+            resizeMode='contain'
+          />
+
+          <View className='relative mt-5'>
+            <Text className='text-3xl text-white font-bold text-center'>
+              Poznaj region{'\n'}i zachwyć się pięknem{' '}
+              <Text className='text-red'>Gór Świętokrzyskich</Text>
+            </Text>
+          </View>
+
+          <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>
+            Zdobywaj najwyższe szczyty w najstarszych polskich górach!
+          </Text>
+
+          <TouchableOpacity
+            onPress={() => router.push('./sign-in')}
+            className='w-full bg-green rounded-2xl m-8 p-5'
+          >
+            <Text className='text-2xl font-mtbold text-gray-100 text-center'>
+              Zaloguj się
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,42 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
         primary: {
-          DEFAULT: "#eef7eb"
+          DEFAULT: '#eef7eb',
         },
-        primaryBG: "#272724",
+        primaryBG: '#272724',
         secondary: {
-          DEFAULT: "#d99e1a",
-          100: "#ecc976",
-          200: "#da9c07",
+          DEFAULT: '#d99e1a',
+          100: '#ecc976',
+          200: '#da9c07',
         },
         black: {
-          DEFAULT: "#000",
-          100: "#1E1E2D",
-          200: "#232533",
+          DEFAULT: '#000',
+          100: '#1E1E2D',
+          200: '#232533',
         },
         gray: {
-          100: "#CDCDE0",
+          100: '#CDCDE0',
         },
-        red: "#ea1b25",
-        green: "#2a8c16"
+        red: '#ea1b25',
+        green: '#2a8c16',
       },
       fontFamily: {
-        mtthin: ["Montserrat-Thin", "sans-serif"],
-        mtextralight: ["Montserrat-ExtraLight", "sans-serif"],
-        mtlight: ["Montserrat-Light", "sans-serif"],
-        mtregular: ["Montserrat-Regular", "sans-serif"],
-        mtmedium: ["Montserrat-Medium", "sans-serif"],
-        mtsemibold: ["Montserrat-SemiBold", "sans-serif"],
-        mtbold: ["Montserrat-Bold", "sans-serif"],
-        mtextrabold: ["Montserrat-ExtraBold", "sans-serif"],
-        mtblack: ["Montserrat-Black", "sans-serif"],
+        mtthin: ['Montserrat-Thin', 'sans-serif'],
+        mtextralight: ['Montserrat-ExtraLight', 'sans-serif'],
+        mtlight: ['Montserrat-Light', 'sans-serif'],
+        mtregular: ['Montserrat-Regular', 'sans-serif'],
+        mtmedium: ['Montserrat-Medium', 'sans-serif'],
+        mtsemibold: ['Montserrat-SemiBold', 'sans-serif'],
+        mtbold: ['Montserrat-Bold', 'sans-serif'],
+        mtextrabold: ['Montserrat-ExtraBold', 'sans-serif'],
+        mtblack: ['Montserrat-Black', 'sans-serif'],
       },
     },
   },
   plugins: [],
-}
-
+};
