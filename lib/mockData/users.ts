e645b776d9756af816c6ab5de4a22d534d38e5c1
Darek Mazur <dev@darekmazur.it>
2024-09-27 16:27:19 +0200

    Add new fields to user object

--- lib/mockData/users.ts
+++ lib/mockData/users.ts
@@ -1,7 +1,49 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { faker } from '@faker-js/faker';
+
 export default [
   {
+    id: faker.string.uuid(),
     username: 'testUser',
     email: 'tu@e.mail',
     password: '123456',
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    avatar: faker.image.avatar(),
+    joinAt: faker.date.past(),
+    description: faker.person.bio(),
+  },
+  {
+    id: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    avatar: faker.image.avatar(),
+    joinAt: faker.date.past(),
+    description: faker.person.bio(),
+  },
+  {
+    id: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    avatar: faker.image.avatar(),
+    joinAt: faker.date.past(),
+    description: faker.person.bio(),
+  },
+  {
+    id: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    avatar: faker.image.avatar(),
+    joinAt: faker.date.past(),
+    description: faker.person.bio(),
   },
 ];
