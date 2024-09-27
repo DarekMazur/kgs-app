9bf3de74969e2d0c4d8b1f2e237732629edaa30e
Darek Mazur <dev@darekmazur.it>
2024-09-27 20:06:17 +0200

    Style post card

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -11,15 +11,19 @@ interface IPostCardProps {
 const PostCard: FC<IPostCardProps> = ({ author, date, title, photoUrl }) => {
   const formatedDate = date.toString();
   return (
-    <View>
-      <Text>przez {author}</Text>
+    <View className='w-full pt-3 pb-10 px-5 m-3 bg-gray-100'>
+      <Text className='text-center text-xl font-mtsemibold'>
+        przez {author}
+      </Text>
       <Image
         source={{ uri: photoUrl }}
-        className='w-[300px] h-[500px]'
+        className='w-[200px] h-[250px] self-center m-3'
         resizeMode='cover'
       />
-      <Text>{formatedDate}</Text>
-      <Text>{title}</Text>
+      <View className='self-end items-end'>
+        <Text className='py-2 text-3xl'>{title}</Text>
+        <Text>{formatedDate}</Text>
+      </View>
     </View>
   );
 };
