cc526e2edf48510c046a1d50786076ef64a46215
Darek Mazur <dev@darekmazur.it>
2024-09-30 23:52:29 +0200

    Create mock api for peaks

--- mocks/handlers/peaks.ts
+++ mocks/handlers/peaks.ts
@@ -6,4 +6,11 @@ export const handlers = [
   http.get('/api/peaks', () => {
     return HttpResponse.json(db.peak.getAll());
   }),
+
+  http.get('/api/peaks/:peakId', async ({ params }) => {
+    const { peakId } = params;
+    return HttpResponse.json(
+      db.peak.getAll().filter((post) => post.id === peakId),
+    );
+  }),
 ];
