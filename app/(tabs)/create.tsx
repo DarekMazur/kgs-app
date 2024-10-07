84bc440c1f4f0d8603dc9b251cab9fd8cd3f5413
Darek Mazur <dev@darekmazur.it>
2024-10-07 04:20:05 +0200

    Add basic method to check location

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -7,10 +7,12 @@ import {
   TouchableOpacity,
   Image,
   Animated,
+  Alert,
 } from 'react-native';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import uuid from 'react-native-uuid';
 import { router, useFocusEffect } from 'expo-router';
+import * as Location from 'expo-location';
 import ButtonCustom from '@/components/ButtonCustom';
 import { icons } from '@/constants';
 import { createPost, getAllPeaks } from '@/lib/getDataFromApi';
@@ -18,6 +20,7 @@ import useApi from '@/hooks/useApi';
 import { useGlobalContext } from '@/context/GlobalProvider';
 import InputCustom from '@/components/InputCustom';
 import ScrollView = Animated.ScrollView;
+import getDistance from '@/lib/helpers/getDistance';
 
 const initialPostData = {
   id: '',
@@ -33,6 +36,12 @@ const initialPostData = {
   peak: null,
 };
 
+interface IDistancesArrayElem {
+  id: string;
+  name: string;
+  dist: number;
+}
+
 const createScreen = () => {
   const { data: peaks, refetch } = useApi(getAllPeaks);
   const { user } = useGlobalContext();
@@ -41,10 +50,25 @@ const createScreen = () => {
   const [isCameraActive, setIsCameraActive] = useState<boolean>(false);
   const cameraRef = useRef(null);
   const [postData, setPostData] = useState(initialPostData);
+  const [location, setLocation] = useState(null);
+  const [distances, setDistances] = useState<IDistancesArrayElem[]>([]);
 
   const onRefresh = async () => {
     await refetch();
   };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Błąd...', 'Permission to access location was denied');
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
 
   useFocusEffect(
     useCallback(() => {
@@ -69,21 +93,58 @@ const createScreen = () => {
         },
         peak: randomPeak,
       });
+
+      if (location) {
+        const allDistances = [];
+        peaks.forEach((peak, index) => {
+          const dist = getDistance(
+            location.coords.latitude,
+            location.coords.altitude,
+            peak.localizationLat,
+            peak.localizationLng,
+          ).toFixed(2);
+
+          const peakDistance = {
+            id: peak.id,
+            name: peak.name,
+            dist,
+          };
+
+          allDistances.push(peakDistance);
+        });
+        const sorted = allDistances.sort((a, b) => a.dist - b.dist);
+        setDistances(sorted);
+      }
     }
-  }, [peaks]);
+  }, [peaks, location]);
 
   if (!permission) {
-    return <View />;
+    return <SafeAreaView />;
   }
 
   if (!permission.granted) {
     return (
-      <View>
+      <SafeAreaView className='bg-primaryBG text-primary w-full'>
         <Text className='text-xl text-secondary text-center'>
           We need your permission to show the camera
         </Text>
         <Button onPress={requestPermission} title='grant permission' />
-      </View>
+      </SafeAreaView>
+    );
+  }
+
+  if (distances.length > 0 && distances[0].dist >= 0.5) {
+    return (
+      <SafeAreaView className='bg-primaryBG text-primary w-full h-full items-center justify-center'>
+        <Text className='text-2xl text-red text-center p-4'>
+          No peaks close enough!
+        </Text>
+        <ButtonCustom
+          title='Go back'
+          handlePress={() => router.back()}
+          containerStyles='w-[50%]'
+        />
+      </SafeAreaView>
     );
   }
 
@@ -116,6 +177,11 @@ const createScreen = () => {
           <View className='items-center justify-center'>
             <Text className='text-xl text-secondary text-center'>
               Create new post
+            </Text>
+            <Text className='text-xl text-secondary text-center'>
+              {distances.length > 0
+                ? `Nearest peak: ${distances[0].name} (${distances[0].dist} km)`
+                : null}
             </Text>
           </View>
           <InputCustom
