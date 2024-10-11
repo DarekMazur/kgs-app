56476f0db7e7ad40bd454062a114ed117a972fa1
Darek Mazur <dev@darekmazur.it>
2024-10-11 03:52:12 +0200

    Fix peak query types

--- app/peak/[query].tsx
+++ app/peak/[query].tsx
@@ -6,6 +6,15 @@ import useApi from '@/hooks/useApi';
 import { getAllPosts, getSinglePeak } from '@/lib/getDataFromApi';
 import Loader from '@/components/Loader';
 import { formatDate } from '@/lib/helpers';
+import { IPeakProps, IPostsProps } from '@/lib/types';
+
+interface IConquerorsType {
+  id: string;
+  username: string;
+  firstName: string;
+  avatar: string;
+  time: Date;
+}
 
 const peak = () => {
   const { query } = useLocalSearchParams();
@@ -13,15 +22,15 @@ const peak = () => {
     getSinglePeak(query as string),
   );
   const { data: posts, loading: postsLoading } = useApi(getAllPosts);
-  const [hikers, setHikers] = useState([]);
+  const [hikers, setHikers] = useState<IConquerorsType[]>([]);
 
   useEffect(() => {
     if (posts && singlePeak) {
       // eslint-disable-next-line array-callback-return
-      const peakPosts = posts.filter(
-        (post) => post.peak.id === singlePeak[0].id,
+      const peakPosts = (posts as IPostsProps[]).filter(
+        (post) => (post.peak as IPeakProps).id === singlePeak[0].id,
       );
-      const peakConquerors = [];
+      const peakConquerors: IConquerorsType[] = [];
 
       peakPosts.forEach((post) =>
         peakConquerors.push({ ...post.author, time: post.createdAt }),
@@ -38,18 +47,19 @@ const peak = () => {
         <ScrollView className='m-4'>
           <View className='p-3'>
             <Text className='text-primary font-mtblack text-3xl text-center pb-2'>
-              {singlePeak[0].name} - {singlePeak[0].height} m
+              {(singlePeak as IPeakProps[])[0].name} -{' '}
+              {(singlePeak as IPeakProps[])[0].height} m
             </Text>
             <Image
-              source={{ uri: singlePeak[0].image }}
+              source={{ uri: (singlePeak as IPeakProps[])[0].image }}
               className='w-full h-[300px] self-center my-4'
               resizeMode='cover'
             />
             <Text className='text-secondary text-sm self-end mb-3'>
-              {singlePeak[0].trial}
+              {(singlePeak as IPeakProps[])[0].trial}
             </Text>
             <Text className='text-primary text-lg my-4 leading-6'>
-              {singlePeak[0].description}
+              {(singlePeak as IPeakProps[])[0].description}
             </Text>
           </View>
           <View>
