7fd2683c173040ee72d2ac6fce7dc51e4cc57694
Darek Mazur <dev@darekmazur.it>
2024-10-20 21:11:11 +0200

    Create single post admin screen sketch

--- app/admin/post/[query].tsx
+++ app/admin/post/[query].tsx
@@ -1,13 +1,50 @@
-import { Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useLocalSearchParams } from 'expo-router';
+import { useEffect, useState } from 'react';
+import useApi from '@/hooks/useApi';
+import { getSinglePost } from '@/lib/getDataFromApi';
+import { IPostsProps } from '@/lib/types';
+import Loader from '@/components/Loader';
+import IconButton from '@/components/IconButton';
+import { icons } from '@/constants';
 
 const adminPostEdit = () => {
   const { query } = useLocalSearchParams();
+  const { data, loading } = useApi(() => getSinglePost(query as string));
+  const [postData, setPostData] = useState<IPostsProps | undefined>();
+
+  useEffect(() => {
+    if (data) {
+      setPostData((data as IPostsProps[])[0]);
+    }
+  }, [data]);
 
   return (
     <SafeAreaView>
-      <Text>Post query: {query}</Text>
+      <Loader isLoading={loading} />
+      {postData ? (
+        <>
+          <View>
+            <Text>{`${postData.peak?.name} ${postData.author.username}`}</Text>
+            <Image
+              source={{ uri: postData.photo }}
+              className='w-full h-[200px]'
+              resizeMode='cover'
+            />
+          </View>
+          <Text>{postData.notes}</Text>
+        </>
+      ) : null}
+      <View>
+        <IconButton icon={icons.hidden} onPress={() => {}} title='Ukryj wpis' />
+        <IconButton
+          icon={icons.suspended}
+          onPress={() => {}}
+          title='Ostrzeżenie'
+        />
+        <IconButton icon={icons.banned} onPress={() => {}} title='Ban' />
+      </View>
     </SafeAreaView>
   );
 };
