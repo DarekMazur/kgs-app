e546218885209b899e40f29ae599aaa9a7bbed6a
Darek Mazur <dev@darekmazur.it>
2024-11-15 01:41:53 +0100

    Update api links

--- mocks/handlers/users.ts
+++ mocks/handlers/users.ts
@@ -32,7 +32,7 @@ export const handlers = [
     }
   }),
 
-  http.get('/login', ({ request }) => {
+  http.get(`${process.env.EXPO_PUBLIC_API_URL}/users/login`, ({ request }) => {
     // @ts-expect-error
     if (request.body && request.body.email) {
       const loggedUser = db.user.findFirst({
@@ -73,39 +73,42 @@ export const handlers = [
     return HttpResponse.json('Request failed', { status: 400 });
   }),
 
-  http.get('/current', ({ request }) => {
-    // @ts-expect-error
-    const token = request.headers.map.authorization?.split(' ')[1];
+  http.get(
+    `${process.env.EXPO_PUBLIC_API_URL}/users/current`,
+    ({ request }) => {
+      // @ts-expect-error
+      const token = request.headers.map.authorization?.split(' ')[1];
 
-    if (!token) {
-      return HttpResponse.json('Invalid or expired token', { status: 403 });
-    }
-
-    try {
-      const decode = JWT.decode(
-        token,
-        process.env.EXPO_PUBLIC_SECRET_KEY as string,
-      );
-
-      if (!decode) {
-        return HttpResponse.json('Authentication failed', { status: 403 });
+      if (!token) {
+        return HttpResponse.json('Invalid or expired token', { status: 403 });
       }
 
-      const { id } = decode;
+      try {
+        const decode = JWT.decode(
+          token,
+          process.env.EXPO_PUBLIC_SECRET_KEY as string,
+        );
 
-      const currentUser = db.user.findFirst({
-        where: {
-          id: {
-            equals: id,
+        if (!decode) {
+          return HttpResponse.json('Authentication failed', { status: 403 });
+        }
+
+        const { id } = decode;
+
+        const currentUser = db.user.findFirst({
+          where: {
+            id: {
+              equals: id,
+            },
           },
-        },
-      });
+        });
 
-      return HttpResponse.json(currentUser);
-    } catch (error) {
-      return HttpResponse.json('Authentication failed', { status: 403 });
-    }
-  }),
+        return HttpResponse.json(currentUser);
+      } catch (error) {
+        return HttpResponse.json('Authentication failed', { status: 403 });
+      }
+    },
+  ),
 
   // http.get(`${process.env.EXPO_PUBLIC_API_URL}/users`, () => {
   //   return HttpResponse.json(db.user.getAll());
