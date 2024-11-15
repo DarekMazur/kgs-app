085f521671cf9b2aa07639a8e61c04c5f4ffb14b
Darek Mazur <dev@darekmazur.it>
2024-11-15 14:36:15 +0100

    Update posts mock api

--- mocks/handlers/posts.ts
+++ mocks/handlers/posts.ts
@@ -1,154 +1,179 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { http, HttpResponse } from 'msw';
+import JWT from 'expo-jwt';
 import { db } from '@/mocks/db';
 import { IPublicPost } from '@/lib/types';
 import { constants } from '@/constants';
 
 export const handlers = [
-  http.get(`${process.env.EXPO_PUBLIC_API_URL}/posts`, () => {
-    return HttpResponse.json(
-      db.post.getAll().sort((a, b) => {
-        if (b.createdAt > a.createdAt) {
-          return 1;
-        }
+  http.get(`${process.env.EXPO_PUBLIC_API_URL}/posts`, ({ request }) => {
+    // @ts-expect-error
+    const token = request.headers.map.authorization?.split(' ')[1];
 
-        if (a.createdAt > b.createdAt) {
-          return -1;
-        }
+    if (!token) {
+      return HttpResponse.json('Invalid or expired token', { status: 403 });
+    }
 
-        return 0;
-      }),
-    );
-  }),
-
-  http.get(
-    `${process.env.EXPO_PUBLIC_API_URL}/posts/:postId`,
-    async ({ params }) => {
-      const { postId } = params;
-      return HttpResponse.json(
-        db.post.getAll().filter((post) => post.id === postId),
+    try {
+      const decode = JWT.decode(
+        token,
+        process.env.EXPO_PUBLIC_SECRET_KEY as string,
       );
-    },
-  ),
 
-  http.post(`${process.env.EXPO_PUBLIC_API_URL}/posts`, async ({ request }) => {
-    const newPost = (await request.json()) as IPublicPost;
-    const createdTime = new Date(Date.now());
+      if (!decode) {
+        return HttpResponse.json('Authentication failed', { status: 403 });
+      }
 
-    const user = db.user.findFirst({
-      where: {
-        id: {
-          equals: newPost.author.id as string,
-        },
-      },
-    })!;
-
-    const newPostData = {
-      id: newPost.id,
-      createdAt: createdTime,
-      author: {
-        id: user.id,
-        username: user.username,
-        firstName: user.firstName,
-        avatar: user.avatar,
-        isSuspended: constants.suspensionConditions(user.suspensionTimeout),
-        isBanned: user.isBanned,
-      },
-      notes: newPost.notes,
-      photo: newPost.photo,
-      isHidden: false,
-      peak: db.peak.findFirst({
-        where: {
-          id: {
-            equals: newPost.peak?.id as string,
-          },
-        },
-      })!,
-    };
-
-    db.post.create(newPostData);
-    db.user.update({
-      where: {
-        id: {
-          equals: user.id,
-        },
-      },
-      data: {
-        posts: [...user.posts, newPostData],
-      },
-    });
-
-    return HttpResponse.json(newPostData, { status: 201 });
+      return HttpResponse.json(db.post.getAll());
+    } catch (error) {
+      return HttpResponse.json('Authentication failed', { status: 403 });
+    }
   }),
 
-  http.put(
-    `${process.env.EXPO_PUBLIC_API_URL}/posts/:postId`,
-    // eslint-disable-next-line consistent-return
-    async ({ request }) => {
-      const updatedPost = (await request.json()) as IPublicPost;
-
-      if (updatedPost) {
-        db.post.update({
-          where: {
-            id: {
-              equals: updatedPost.id,
-            },
-          },
-          data: {
-            notes: updatedPost.notes,
-            isHidden: updatedPost.isHidden,
-          },
-        });
-
-        return HttpResponse.json(updatedPost, { status: 201 });
-      }
-    },
-  ),
-
-  http.delete(
-    `${process.env.EXPO_PUBLIC_API_URL}/posts/:postId`,
-    async ({ params }) => {
-      const { postId } = params;
-
-      if (postId) {
-        const post = db.post.findFirst({
-          where: {
-            id: {
-              equals: postId as string,
-            },
-          },
-        })!;
-
-        const user = db.user.findFirst({
-          where: {
-            id: {
-              equals: post.author.id as string,
-            },
-          },
-        })!;
-
-        db.post.delete({
-          where: {
-            id: {
-              equals: postId as string,
-            },
-          },
-        });
-
-        db.user.update({
-          where: {
-            id: {
-              equals: user.id as string,
-            },
-          },
-          data: {
-            posts: user.posts.filter((userPost) => userPost.id !== postId),
-          },
-        });
-        return HttpResponse.json();
-      }
-
-      return new HttpResponse(null, { status: 404 });
-    },
-  ),
+  //   http.get(`${process.env.EXPO_PUBLIC_API_URL}/posts`, () => {
+  //     return HttpResponse.json(
+  //       db.post.getAll().sort((a, b) => {
+  //         if (b.createdAt > a.createdAt) {
+  //           return 1;
+  //         }
+  //
+  //         if (a.createdAt > b.createdAt) {
+  //           return -1;
+  //         }
+  //
+  //         return 0;
+  //       }),
+  //     );
+  //   }),
+  //
+  //   http.get(
+  //     `${process.env.EXPO_PUBLIC_API_URL}/posts/:postId`,
+  //     async ({ params }) => {
+  //       const { postId } = params;
+  //       return HttpResponse.json(
+  //         db.post.getAll().filter((post) => post.id === postId),
+  //       );
+  //     },
+  //   ),
+  //
+  //   http.post(`${process.env.EXPO_PUBLIC_API_URL}/posts`, async ({ request }) => {
+  //     const newPost = (await request.json()) as IPublicPost;
+  //     const createdTime = new Date(Date.now());
+  //
+  //     const user = db.user.findFirst({
+  //       where: {
+  //         id: {
+  //           equals: newPost.author.id as string,
+  //         },
+  //       },
+  //     })!;
+  //
+  //     const newPostData = {
+  //       id: newPost.id,
+  //       createdAt: createdTime,
+  //       author: {
+  //         id: user.id,
+  //         username: user.username,
+  //         firstName: user.firstName,
+  //         avatar: user.avatar,
+  //         isSuspended: constants.suspensionConditions(user.suspensionTimeout),
+  //         isBanned: user.isBanned,
+  //       },
+  //       notes: newPost.notes,
+  //       photo: newPost.photo,
+  //       isHidden: false,
+  //       peak: db.peak.findFirst({
+  //         where: {
+  //           id: {
+  //             equals: newPost.peak?.id as string,
+  //           },
+  //         },
+  //       })!,
+  //     };
+  //
+  //     db.post.create(newPostData);
+  //     db.user.update({
+  //       where: {
+  //         id: {
+  //           equals: user.id,
+  //         },
+  //       },
+  //       data: {
+  //         posts: [...user.posts, newPostData],
+  //       },
+  //     });
+  //
+  //     return HttpResponse.json(newPostData, { status: 201 });
+  //   }),
+  //
+  //   http.put(
+  //     `${process.env.EXPO_PUBLIC_API_URL}/posts/:postId`,
+  //     // eslint-disable-next-line consistent-return
+  //     async ({ request }) => {
+  //       const updatedPost = (await request.json()) as IPublicPost;
+  //
+  //       if (updatedPost) {
+  //         db.post.update({
+  //           where: {
+  //             id: {
+  //               equals: updatedPost.id,
+  //             },
+  //           },
+  //           data: {
+  //             notes: updatedPost.notes,
+  //             isHidden: updatedPost.isHidden,
+  //           },
+  //         });
+  //
+  //         return HttpResponse.json(updatedPost, { status: 201 });
+  //       }
+  //     },
+  //   ),
+  //
+  //   http.delete(
+  //     `${process.env.EXPO_PUBLIC_API_URL}/posts/:postId`,
+  //     async ({ params }) => {
+  //       const { postId } = params;
+  //
+  //       if (postId) {
+  //         const post = db.post.findFirst({
+  //           where: {
+  //             id: {
+  //               equals: postId as string,
+  //             },
+  //           },
+  //         })!;
+  //
+  //         const user = db.user.findFirst({
+  //           where: {
+  //             id: {
+  //               equals: post.author.id as string,
+  //             },
+  //           },
+  //         })!;
+  //
+  //         db.post.delete({
+  //           where: {
+  //             id: {
+  //               equals: postId as string,
+  //             },
+  //           },
+  //         });
+  //
+  //         db.user.update({
+  //           where: {
+  //             id: {
+  //               equals: user.id as string,
+  //             },
+  //           },
+  //           data: {
+  //             posts: user.posts.filter((userPost) => userPost.id !== postId),
+  //           },
+  //         });
+  //         return HttpResponse.json();
+  //       }
+  //
+  //       return new HttpResponse(null, { status: 404 });
+  //     },
+  //   ),
 ];
