bb9a503ad3d72ee8a60abfebcf2f00d46c1f3808
Darek Mazur <dev@darekmazur.it>
2024-11-15 02:13:42 +0100

    Update edit user mock api

--- mocks/handlers/users.ts
+++ mocks/handlers/users.ts
@@ -188,6 +188,76 @@ export const handlers = [
     return HttpResponse.json(newUserData, { status: 201 });
   }),
 
+  http.put(
+    `${process.env.EXPO_PUBLIC_API_URL}/users/:userId`,
+    async ({ request, params }) => {
+      // @ts-expect-error
+      const token = request.headers.map.authorization?.split(' ')[1];
+      const { userId } = params;
+
+      if (!token) {
+        return HttpResponse.json('Invalid or expired token', { status: 403 });
+      }
+
+      if (!userId) {
+        return HttpResponse.json('Request failed', { status: 400 });
+      }
+
+      try {
+        const decode = JWT.decode(
+          token,
+          process.env.EXPO_PUBLIC_SECRET_KEY as string,
+        );
+
+        if (!decode) {
+          return HttpResponse.json('Authentication failed', { status: 403 });
+        }
+
+        const user = db.user.findFirst({
+          where: {
+            id: {
+              equals: decode.id as string,
+            },
+          },
+        });
+
+        if (user) {
+          db.user.update({
+            where: {
+              id: {
+                equals: user.id as string,
+              },
+            },
+            data: {
+              username: user.username as string,
+              password: user.password,
+              firstName: user.firstName,
+              lastName: user.lastName,
+              avatar: user.avatar,
+              description: user.description,
+              isBanned: user.isBanned,
+              totalSuspensions: user.totalSuspensions,
+              suspensionTimeout: user.suspensionTimeout,
+              isConfirmed: true,
+              role: db.role.findFirst({
+                where: {
+                  id: {
+                    equals: user.role?.id,
+                  },
+                },
+              })!,
+            },
+          });
+
+          return HttpResponse.json(user, { status: 200 });
+        }
+        return HttpResponse.json('User not found', { status: 404 });
+      } catch (error) {
+        return HttpResponse.json('Authentication failed', { status: 403 });
+      }
+    },
+  ),
+
   // http.get(`${process.env.EXPO_PUBLIC_API_URL}/users`, () => {
   //   return HttpResponse.json(db.user.getAll());
   // }),
