777e2d38ad86a63565cb3fcf643b089e9f253a98
Darek Mazur <dev@darekmazur.it>
2024-11-15 21:59:01 +0100

    Update roles mock api

--- mocks/handlers/roles.ts
+++ mocks/handlers/roles.ts
@@ -1,9 +1,30 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { http, HttpResponse } from 'msw';
+import JWT from 'expo-jwt';
 import { db } from '@/mocks/db';
 
 export const handlers = [
-  http.get(`${process.env.EXPO_PUBLIC_API_URL}/roles`, () => {
-    return HttpResponse.json(db.role.getAll());
+  http.get(`${process.env.EXPO_PUBLIC_API_URL}/roles`, ({ request }) => {
+    // @ts-expect-error
+    const token = request.headers.map.authorization?.split(' ')[1];
+
+    if (!token) {
+      return HttpResponse.json('Invalid or expired token', { status: 403 });
+    }
+
+    try {
+      const decode = JWT.decode(
+        token,
+        process.env.EXPO_PUBLIC_SECRET_KEY as string,
+      );
+
+      if (!decode) {
+        return HttpResponse.json('Authentication failed', { status: 403 });
+      }
+
+      return HttpResponse.json(db.role.getAll(), { status: 200 });
+    } catch (error) {
+      return HttpResponse.json('Authentication failed', { status: 403 });
+    }
   }),
 ];
